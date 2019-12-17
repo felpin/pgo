5bd53395f5a81d1e79bf245964d5aca0f836de03
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-12-17 00:12:01 -0300

    Use styled components instead of styling with CSS

---
+++ src/App.styles.ts
@@ -0,0 +1,38 @@
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Container = styled.div`
+  text-align: center;
+`;
+
+export const Header = styled.header`
+  align-items: center;
+  background-color: #282c34;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-size: calc(10px + 2vmin);
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+export const Link = styled.a`
+  color: #61dafb;
+`;
+
+export const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${spin} infinite 20s linear;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import { Container, Header, Link, Logo } from './App.styles';
 import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <Header>
+        <Logo alt="logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <Link href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Header>
+    </Container>
   );
 };
 
